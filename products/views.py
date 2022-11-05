9be2c1a9f0ea5bac1a30b60ede5a627d8260c424
Kanat <113922116+iprogrammer1390@users.noreply.github.com>
2022-11-06 00:43:48 +0600

    Update views.py

--- products/views.py
+++ products/views.py
@@ -15,7 +15,7 @@ class IndexView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        r = requests.get('https://suppliers-api.wildberries.ru/public/api/v1/info', headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjRmODE0NDA3LWI3ZWQtNDVlNS04MzgxLWNlN2QyNTcwNWRhYiJ9.7M5xJmAZEBRcoEcZukw6ae-7VamUqmPGKNzei3Zj4KQ'})
+        r = requests.get('https://suppliers-api.wildberries.ru/public/api/v1/info', headers={'Authorization': 'ciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjRmODE0NDA3LWI3ZWQtNDVlNS04MzgxLWNlN2QyNTcwNWRhYiJ9.7M5xJmAZEBRcoEcZukw6ae-7VamUqmPGKNzei3Zj4KQ'})
         data = r.json()
         prices = {str(item['nmId']):(int(item['price'] - item['price']*item['discount']/100)) for item in data}
         context['prices'] = prices
